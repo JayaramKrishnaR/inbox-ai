8349ad3fae280e57bb125f0d39458520ef85161e
JayaramKrishnaR <JayaramKrishnaR@users.noreply.github.com>
2025-09-05 20:19:05 +0530

    initial commit

---
+++ email-ingestion-service/email-ingestion-service/src/main/java/com/inbox/ai/ingestion/EmailIngestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inbox.ai.ingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailIngestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailIngestionServiceApplication.class, args);
+	}
+
+}
---
+++ email-ingestion-service/email-ingestion-service/src/test/java/com/inbox/ai/ingestion/EmailIngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inbox.ai.ingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailIngestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
