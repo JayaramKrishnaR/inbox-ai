b353cc198eb2114c7125e9df9eb60581f58966ca
JayaramKrishnaR <JayaramKrishnaR@users.noreply.github.com>
2025-09-05 23:11:09 +0530

    email-ai-service initial commit

---
+++ email-ai-service/src/main/java/com/inbox/ai/service/EmailAiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inbox.ai.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAiServiceApplication.class, args);
+	}
+
+}
